4cea137f29613dd51a0baefb81edda68d999fde6
Ashraful haque <60834909+Ashraful34@users.noreply.github.com>
2025-05-18 21:57:15 +0600

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,56 @@
+// Arrays
+
+// let marks =[97,82,64,75,36];
+
+// console.log(marks);
+
+// const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+// console.log(shopping);
+
+// const sequence = [1, 1, 2, 3, 5, 8, 13];
+// const random = ["tree", 795, [0, 1, 2]];
+
+// console.log(sequence);
+// console.log(random);
+
+// console.log(shopping[0]);  //bread
+
+// shopping[0] = "tahini";
+// console.log(shopping);
+
+// const cities = ["Manchester","Liverpool"];
+// cities.push("Cardif");  //To add one or more items to the end of an array
+// console.log(cities);    
+
+// cities.push("Bradford","Brighton");
+// console.log(cities);
+
+// const newLength = cities.push("Bristol");
+// console.log(newLength);
+
+
+// To add an item to the start of the array
+
+// const cities = ["Manchester","Liverpool"];
+// console.log(cities);
+// cities.unshift("Edinburgh");
+// console.log(cities);
+
+// To remove the last item from the array
+
+// cities.pop();
+// console.log(cities);
+
+// To remove the first item from an array
+
+// cities.shift();
+// console.log(cities);
+
+// let arr = [1,2,3,4,5,6,7];
+
+// let ar2 = arr.slice(2,3);
+
+// console.log(ar2);
+
+//  arr.splice(2,2,101,102);
+// console.log(arr);
